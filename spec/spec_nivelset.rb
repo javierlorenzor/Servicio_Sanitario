9b10ee9c3d302fd71df9d0478a49cbc339a60307
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-05 21:56:58 +0000

    pruebas getter (fallo)

--- spec/spec_nivelset.rb
+++ spec/spec_nivelset.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 require 'ServicioSanitario/NivelSet'
 
 RSpec.describe ServicioSanitario::NivelSet do
-    it "Se espera que se asigne el nivel , categoría y tiempo correcto" do
-        nivel_set = ServicioSanitario::NivelSet.new(:I, "Reanimacion", "Inmediato")
-        expect(nivel_set.nivel).to eq(:I)
-        expect(nivel_set.categoria).to eq("Reanimacion")
-        expect(nivel_set.tiempo_atencion).to eq("Inmediato")
+
+    it "Se espera poder crear una instancia de NivelSet" do
+        nivel_set = ServicioSanitario::NivelSet.new(:I, 'Reanimacion', 'Inmediato')
+        expect(nivel_set).not_to be_nil
     end
+
+    it "Se espera que se pueda obtener el valor correcto de nivel" do
+        nivel_set = ServicioSanitario::NivelSet.new(:I, 'Reanimacion', 'Inmediato')
+        expect(nivel_set.nivel).to eq(:I)
+        expect(nivel_set.categoria).to eq('Reanimacion')
+        expect(nivel_set.tiempo_atencion).to eq('Inmediato')
+    end
+    
 end
