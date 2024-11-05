3390240d1d7db1daef701201c3733230d8676790
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-05 21:41:49 +0000

    implementación initialize (no fallo)

--- lib/ServicioSanitario/NivelSet.rb
+++ lib/ServicioSanitario/NivelSet.rb
@@ -1,5 +1,10 @@
 module ServicioSanitario
     class NivelSet
-
+        attr_reader :nivel, :categoria, :tiempo_atencion
+        def initialize(nivel, categoria, tiempo_atencion)
+            @nivel = nivel
+            @categoria = categoria
+            @tiempo_atencion = tiempo_atencion
+        end
     end
 end
