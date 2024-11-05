98f7ca5e2dd9b8ac5e4be470e3ad997ff680d392
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-05 23:30:44 +0000

    implementacion initialize(no fallo)

--- lib/ServicioSanitario/Horas.rb
+++ lib/ServicioSanitario/Horas.rb
@@ -1,4 +1,10 @@
 module ServicioSanitario
     class Hora
+        def initialize(hora:, minuto:, segundo:)
+            @hora = hora
+            @minuto = minuto
+            @segundo = segundo
+          end
+      
     end
 end  
