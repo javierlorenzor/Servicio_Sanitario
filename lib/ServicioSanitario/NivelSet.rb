d95b881f6dbbaecaf893bf46d6fac57bc1a7f1f2
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-05 21:41:27 +0000

    pruebas initialize (fallo)

--- lib/ServicioSanitario/NivelSet.rb
+++ lib/ServicioSanitario/NivelSet.rb
@@ -1,4 +1,5 @@
 module ServicioSanitario
     class NivelSet
+
     end
 end
--- spec/spec_nivelset.rb
+++ spec/spec_nivelset.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 require 'ServicioSanitario/NivelSet'
 
 RSpec.describe ServicioSanitario::NivelSet do
-    
+    it "Se espera que se asigne el nivel , categoría y tiempo correcto" do
+        nivel_set = ServicioSanitario::NivelSet.new(:I, "Reanimacion", "Inmediato")
+        expect(nivel_set.nivel).to eq(:I)
+        expect(nivel_set.categoria).to eq("Reanimacion")
+        expect(nivel_set.tiempo_atencion).to eq("Inmediato")
+    end
 end
