5cc0a2ef4b2469d5d41701c0e0103ad220878d68
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-05 20:33:20 +0000

    commit inicial

---
+++ lib/ServicioSanitario.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ServicioSanitario/version"
+
+module ServicioSanitario
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ServicioSanitario/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ServicioSanitario
+  VERSION = "0.1.0"
+end
