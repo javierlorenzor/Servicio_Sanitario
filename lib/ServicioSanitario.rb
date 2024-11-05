933399162067b630eca5119885b14cdb17da9a17
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-05 22:12:00 +0000

    implementacion constantes (no fallo)

--- lib/ServicioSanitario.rb
+++ lib/ServicioSanitario.rb
@@ -3,8 +3,12 @@
 require_relative "ServicioSanitario/version"
 
 module ServicioSanitario
-
-
+  AZUL = { nivel: :I, categoria: :Reanimacion, tiempo_atencion: :Inmediato }
+  ROJO = { nivel: :II, categoria: :Emergencia, tiempo_atencion: '7 minutos' }
+  NARANJA = { nivel: :III, categoria: :Urgente, tiempo_atencion: '30 minutos' }
+  VERDE = { nivel: :IV, categoria: :Menos_urgente, tiempo_atencion: '45 minutos' }
+  NEGRO = { nivel: :V, categoria: :No_urgente, tiempo_atencion: '60 minutos' }
+  
   class Error < StandardError; end
   # Your code goes here...
 end
