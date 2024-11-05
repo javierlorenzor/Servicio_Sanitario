27b79c171aa96f341200cfc104a3b5ca9bcd7351
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-05 23:30:17 +0000

    prueba initialize(fallo)

--- spec/spec_horas.rb
+++ spec/spec_horas.rb
@@ -3,5 +3,12 @@ require 'ServicioSanitario/Horas'
 require 'ServicioSanitario'
 
 RSpec.describe ServicioSanitario::Hora do
+    before(:each) do
+        @hora = ServicioSanitario::Hora.new(hora: 12, minuto: 30, segundo: 45)
+    end
+    
+    it "Se espera poder crear una instancia de Hora" do
+        expect(@hora).not_to be_nil
+    end
 
 end
