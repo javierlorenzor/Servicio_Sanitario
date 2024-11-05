a92982570a37cf029bf99033fcbfacba04ef10d9
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-05 22:39:03 +0000

    prueba getter (fallo)

--- spec/spec_fecha.rb
+++ spec/spec_fecha.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
 require 'ServicioSanitario/Fecha'
 
 RSpec.describe ServicioSanitario::Fecha do
-    
+
     before(:each) do
         @fecha = ServicioSanitario::Fecha.new(dia: 15, mes: 11, anio: 2024)
     end
 
     it "Se espera poder crear una instancia de Fecha" do
         expect(@fecha).not_to be_nil
+    end
+
+    it "Se espera poder acceder a los atributos de la fecha (getters)" do
+        expect(@fecha.dia).to eq(15)
+        expect(@fecha.mes).to eq(11)
+        expect(@fecha.anio).to eq(2024)
     end
 end
