86342de853d67748e87435cdf62afee6f34f9c9c
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-05 22:36:20 +0000

    prueba initialize (fallo)

--- spec/spec_fecha.rb
+++ spec/spec_fecha.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 require 'ServicioSanitario/Fecha'
 
 RSpec.describe ServicioSanitario::Fecha do
+    
+    before(:each) do
+        @fecha = ServicioSanitario::Fecha.new(dia: 15, mes: 11, anio: 2024)
+    end
+
+    it "Se espera poder crear una instancia de Fecha" do
+        expect(@fecha).not_to be_nil
+    end
 end
