eac5e631956d97051083aca1efd6bc97fafcdaf5
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-05 22:37:06 +0000

    implementacion  initialize (no fallo)

--- lib/ServicioSanitario/Fecha.rb
+++ lib/ServicioSanitario/Fecha.rb
@@ -1,4 +1,9 @@
 module ServicioSanitario
     class Fecha
+        def initialize(dia:, mes:, anio:)
+            @dia = dia
+            @mes = mes
+            @anio = anio
+        end
     end
 end  
