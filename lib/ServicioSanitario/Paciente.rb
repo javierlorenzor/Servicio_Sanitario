17f5fbb7adbcd2d8cf58f99a4988e0f15e28da1c
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-08 20:53:38 +0000

    initialize  paciente (no fallo)

--- lib/ServicioSanitario/Paciente.rb
+++ lib/ServicioSanitario/Paciente.rb
@@ -3,7 +3,11 @@ require_relative 'NivelSet'
 
 module ServicioSanitario
     class Paciente < Persona
-
+        def initialize(numero_identificacion, nombre, apellido, sexo, fecha_nacimiento, prioridad)
+            super(numero_identificacion, nombre, apellido, sexo, fecha_nacimiento)
+            @prioridad = prioridad
+            @diagnosticos = []
+        end
 
     end
 end
