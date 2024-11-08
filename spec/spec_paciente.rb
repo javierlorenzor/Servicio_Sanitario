e598d51674f88b5fd56a315eed80692e4d219db2
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-08 20:43:01 +0000

    prueba inicial paciente (fallo)

---
+++ spec/spec_paciente.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'ServicioSanitario/Persona'
+require 'ServicioSanitario/NivelSet'
+require 'ServicioSanitario/Paciente'
+require 'ServicioSanitario'
+
+# Descripción de las pruebas para la clase ServicioSanitario::Fecha
+RSpec.describe ServicioSanitario::Paciente do
+    
+end
