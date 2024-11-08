d85e80bbc62b8d51dd967ceb14613b9528c09ae2
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-08 20:10:14 +0000

    implementacion initialize  persona (no fallo)

--- lib/ServicioSanitario/Persona.rb
+++ lib/ServicioSanitario/Persona.rb
@@ -1,5 +1,11 @@
 module ServicioSanitario
     class Persona
-    
+        def initialize(numero_identificacion, nombre, apellido, sexo, fecha_nacimiento)
+            @numero_identificacion = numero_identificacion
+            @nombre = nombre
+            @apellido = apellido
+            @sexo = sexo
+            @fecha_nacimiento = fecha_nacimiento
+          end
     end
 end
