fa07e9d695e8abcef43344c03f19677a4b8f2fd3
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-10 19:13:59 +0000

    initialize medico (fallo)

---
+++ lib/ServicioSanitario/Medico.rb
@@ -0,0 +1,7 @@
+require_relative 'Persona'
+require_relative 'NivelSet'
+
+module ServicioSanitario
+    class Medico < Persona
+    end 
+end
--- spec/spec_medico.rb
+++ spec/spec_medico.rb
@@ -6,5 +6,18 @@ require 'ServicioSanitario'
 
 # Descripción de las pruebas para la clase ServicioSanitario::Medico
 RSpec.describe ServicioSanitario::Medico do
-    
+
+    before(:each) do
+        @fecha1 = ServicioSanitario::Fecha.new(dia: 10, mes: 5, anio: 1980)
+        @medico1 = ServicioSanitario::Medico.new("12345", "Alba", "Perez", "M", @fecha1, "Pediatría")
+        @fecha2 = ServicioSanitario::Fecha.new(dia: 10, mes: 5, anio: 1980)
+        @medico2 = ServicioSanitario::Medico.new("12345", "Miguel", "Tadeo", "M", @fecha2, "Pediatría")
+    end
+
+    it "Se espera que se pueda inicializar persona correctamente con valores válidos" do
+        expect(@medico1).not_to be_nil
+        expect(@medico2).not_to be_nil
+    end
+
+
 end 
