13f699114d645f4a17b4c2eb95c61636e4bf864a
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-10 18:50:23 +0000

    primera prueba medico (fallo)

---
+++ spec/spec_medico.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'ServicioSanitario/Persona'
+require 'ServicioSanitario/NivelSet'
+require 'ServicioSanitario/Paciente'
+require 'ServicioSanitario'
+
+# Descripción de las pruebas para la clase ServicioSanitario::Paciente
+RSpec.describe ServicioSanitario::Medico do
+end 
