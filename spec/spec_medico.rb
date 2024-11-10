f2a1123a0138196e2c1685d59e5dfeaf33a447cd
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-10 19:25:38 +0000

    getters medico (fallo)

--- spec/spec_medico.rb
+++ spec/spec_medico.rb
@@ -9,9 +9,9 @@ RSpec.describe ServicioSanitario::Medico do
 
     before(:each) do
         @fecha1 = ServicioSanitario::Fecha.new(dia: 10, mes: 5, anio: 1980)
-        @medico1 = ServicioSanitario::Medico.new("12345", "Alba", "Perez", "M", @fecha1, "Pediatría")
+        @medico1 = ServicioSanitario::Medico.new("12345", "Alba", "Perez", "F", @fecha1, "Pediatría")
         @fecha2 = ServicioSanitario::Fecha.new(dia: 10, mes: 5, anio: 1980)
-        @medico2 = ServicioSanitario::Medico.new("12345", "Miguel", "Tadeo", "M", @fecha2, "Pediatría")
+        @medico2 = ServicioSanitario::Medico.new("56789", "Miguel", "Tadeo", "M", @fecha2, "Pediatría")
     end
 
     it "Se espera que se pueda inicializar persona correctamente con valores válidos" do
@@ -26,6 +26,16 @@ RSpec.describe ServicioSanitario::Medico do
         expect(@medico1.instance_of?(ServicioSanitario::Medico)).to be true
         expect(ServicioSanitario::Medico.superclass).to eq(ServicioSanitario::Persona)
     end
+
+    it "Se debe inicializar con los atributos correctos" do
+        expect(@medico2.numero_identificacion).to eq("56789")
+        expect(@medico2.nombre).to eq("Miguel")
+        expect(@medico2.apellido).to eq("Tadeo")
+        expect(@medico2.sexo).to eq("M")
+        expect(@medico2.fecha_nacimiento).to eq(@fecha2)
+        expect(@medico2.especialidad).to eq("Pediatría")
+        expect(@medico2.pacientes).to be_empty
+      end
 
 
 end 
