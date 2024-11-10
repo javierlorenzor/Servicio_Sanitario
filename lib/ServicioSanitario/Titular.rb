7e6e3df4018806a668bd86b4466b17ef12caad3b
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-10 20:35:00 +0000

    initialize  titular (no fallo)

--- lib/ServicioSanitario/Titular.rb
+++ lib/ServicioSanitario/Titular.rb
@@ -3,6 +3,9 @@ require_relative 'NivelSet'
 
 module ServicioSanitario
     class Titular < Medico
-        
+        def initialize(numero_identificacion, nombre, apellido, sexo, fecha_nacimiento, especialidad, maximo_pacientes)
+            super(numero_identificacion, nombre, apellido, sexo, fecha_nacimiento, especialidad)
+            @maximo_pacientes = maximo_pacientes
+        end
     end 
 end 
