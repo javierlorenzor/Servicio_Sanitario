8e3298613023df3759e947643a07ec44bb612ead
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-10 19:26:06 +0000

    getters medico (no fallo)

--- lib/ServicioSanitario/Medico.rb
+++ lib/ServicioSanitario/Medico.rb
@@ -3,6 +3,7 @@ require_relative 'NivelSet'
 
 module ServicioSanitario
     class Medico < Persona
+        attr_accessor :especialidad, :pacientes
 
         def initialize(numero_identificacion, nombre, apellido, sexo, fecha_nacimiento, especialidad)
             super(numero_identificacion, nombre, apellido, sexo, fecha_nacimiento)
