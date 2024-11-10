1152eca82b2f1b23783efe21fa5a866915da08ef
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-10 20:27:46 +0000

    primera prueba titular (fallo)

---
+++ spec/spec_titular.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'ServicioSanitario/Titular'
+require 'ServicioSanitario/NivelSet'
+require 'ServicioSanitario/Persona'
+require 'ServicioSanitario/Paciente'
+require 'ServicioSanitario'
+
+# Descripción de las pruebas para la clase ServicioSanitario::Medico
+RSpec.describe ServicioSanitario::Titular do
+end 
