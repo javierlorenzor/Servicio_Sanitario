670de81ee324d2ea738d1041304b9dce854dee0d
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-11 10:03:24 +0000

    getters  titular (no fallo)

--- lib/ServicioSanitario/Titular.rb
+++ lib/ServicioSanitario/Titular.rb
@@ -3,9 +3,9 @@ require_relative 'NivelSet'
 require_relative 'Persona'
 
 module ServicioSanitario
-    attr_accessor :maximo_pacientes
-
     class Titular < Medico
+        attr_accessor :maximo_pacientes
+
         def initialize(numero_identificacion, nombre, apellido, sexo, fecha_nacimiento, especialidad, maximo_pacientes)
             super(numero_identificacion, nombre, apellido, sexo, fecha_nacimiento, especialidad)
             @maximo_pacientes = maximo_pacientes
