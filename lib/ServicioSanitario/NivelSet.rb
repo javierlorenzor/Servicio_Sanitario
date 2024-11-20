8e1d824256c295ebc025224c7a129904a47d6442
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-20 21:14:12 +0000

    enumerable nivel (no fallo)

--- lib/ServicioSanitario/NivelSet.rb
+++ lib/ServicioSanitario/NivelSet.rb
@@ -1,25 +1,24 @@
 module ServicioSanitario
-    # Clase NivelSet que representa el nivel de atención, categoría y tiempo de atención
-    class NivelSet
-      include Comparable
-
-      # Atributos de lectura para el nivel, la categoría y el tiempo de atención
-      attr_reader :nivel, :categoria, :tiempo_atencion
-      
-      def initialize(nivel, categoria, tiempo_atencion)
-        @nivel = nivel
-        @categoria = categoria
-        @tiempo_atencion = tiempo_atencion
-      end
-      
-      def to_s
-        "Nivel: #{@nivel}, Categoría: #{@categoria}, Tiempo de atención: #{@tiempo_atencion}"
-      end
-
-      def <=>(other)
-        tiempo_atencion <=> other.tiempo_atencion
-      end
-      
+  # Clase NivelSet que representa el nivel de atención, categoría y tiempo de atención
+  class NivelSet
+    include Comparable
+    include Enumerable
+    attr_reader :nivel, :categoria, :tiempo_atencion
+    
+    def initialize(nivel, categoria, tiempo_atencion)
+      @nivel = nivel
+      @categoria = categoria
+      @tiempo_atencion = tiempo_atencion
     end
+    
+    def to_s
+      "Nivel: #{@nivel}, Categoría: #{@categoria}, Tiempo de atención: #{@tiempo_atencion}"
+    end
+
+    def <=>(other)
+      tiempo_atencion <=> other.tiempo_atencion
+    end
+    
+  end
 end
   
