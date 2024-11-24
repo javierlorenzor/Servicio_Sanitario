11a12915de681ea4250101c39a3925ead2564704
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-24 18:46:45 +0000

    initialize serviciosalud (no fallo)

--- lib/ServicioSanitario/ServicioSalud.rb
+++ lib/ServicioSanitario/ServicioSalud.rb
@@ -1,5 +1,13 @@
 module ServicioSanitario
     class ServicioSalud
-        
+        def initialize(codigo:, descripcion:, horario_apertura:, horario_cierre:, dias_festivos:, medicos:, camas:)
+            @codigo = codigo
+            @descripcion = descripcion
+            @horario_apertura = horario_apertura # Instancia de Hora
+            @horario_cierre = horario_cierre     # Instancia de Hora
+            @dias_festivos = dias_festivos       # Array de instancias de Fecha
+            @medicos = medicos                   # Array de instancias de Medico
+            @camas = camas                       # Hash con camas y ocupaciones
+        end
     end 
 end 
