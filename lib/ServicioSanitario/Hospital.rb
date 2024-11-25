b3fd2e445c9063f5aed72882d8f607ed98895f01
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-25 21:32:22 +0000

    primera prueba hospital (fallo)

---
+++ lib/ServicioSanitario/Hospital.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'ServicioSanitario/ServicioSalud'
+require 'ServicioSanitario'
+require 'ServicioSanitario/Hospital'
+
+RSpec.describe ServicioSanitario::Hospital do
+
+    context "Inicialización de atributos , to_s y herencia" do
+        it "Se espera poder crear una instancia de ServicioSalud" do
+          expect(ServicioSanitario.Hospital.new).not_to be_nil
+        end
+    end 
+end 
