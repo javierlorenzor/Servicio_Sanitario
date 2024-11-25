1479a8cc575996a2ccdb2b0446d9b40e2db8b7a7
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-25 21:40:22 +0000

    initialize  hospital (no fallo)

--- lib/ServicioSanitario/Hospital.rb
+++ lib/ServicioSanitario/Hospital.rb
@@ -1,4 +1,12 @@
 module ServicioSanitario
     class Hospital < ServicioSalud
+        attr_accessor :numero_plantas
+
+        def initialize(codigo:, descripcion:, horario_apertura:, horario_cierre:, dias_festivos:, medicos:, camas:, numero_plantas:)
+        super(codigo: codigo, descripcion: descripcion, horario_apertura: horario_apertura,
+                horario_cierre: horario_cierre, dias_festivos: dias_festivos, medicos: medicos, camas: camas)
+        @numero_plantas = numero_plantas # Número de plantas del hospital
+        end
+
     end 
 end 
