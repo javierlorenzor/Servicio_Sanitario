b8014c1c913aa488724b1f4121ee0099719c6ad6
javier de Lorenzo <alu0101321257@ull.edu.es>
2024-11-25 21:34:47 +0000

    primera prueba hospital (no fallo)

---
+++ lib/ServicioSanitario/Hospital.rb
@@ -0,0 +1,4 @@
+module ServicioSanitario
+    class Hospital < ServicioSalud
+    end 
+end 
--- spec/spec_hospital.rb
+++ spec/spec_hospital.rb
@@ -7,7 +7,7 @@ RSpec.describe ServicioSanitario::Hospital do
 
     context "Inicialización de atributos , to_s y herencia" do
         it "Se espera poder crear una instancia de ServicioSalud" do
-          expect(ServicioSanitario.Hospital).not_to be_nil
+          expect(ServicioSanitario::Hospital).not_to be_nil
         end
     end 
 end 
